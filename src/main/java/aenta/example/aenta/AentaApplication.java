1ad69b2b07e33498c83da23c72a19df2352c6765
admin admin <admin@admin.com>
2021-10-24 21:05:14 +0100

    Initial commit 24th October 2021

---
+++ src/main/java/aenta/example/aenta/AentaApplication.java
@@ -0,0 +1,13 @@
+package aenta.example.aenta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AentaApplication.class, args);
+	}
+
+}
