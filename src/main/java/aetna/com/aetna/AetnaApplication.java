2eef3b4d2a2653c9da7f1cb944613930f1875db0
Nizam Shariff <36436660+ItsMeNas@users.noreply.github.com>
2021-10-27 19:35:18 +0300

    Add files via upload

---
+++ src/main/java/aetna/com/aetna/AetnaApplication.java
@@ -0,0 +1,13 @@
+package aetna.com.aetna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AetnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AetnaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/aetna/com/aetna/ServletInitializer.java
@@ -0,0 +1,13 @@
+package aetna.com.aetna;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AetnaApplication.class);
+	}
+
+}
