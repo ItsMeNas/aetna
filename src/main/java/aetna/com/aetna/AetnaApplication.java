d28a5c24ec11d1a5620d7f6c20d0f01523da2540
Nizam Shariff <36436660+ItsMeNas@users.noreply.github.com>
2021-10-28 08:47:29 +0300

    Update AetnaApplication.java

--- src/main/java/aetna/com/aetna/AetnaApplication.java
+++ src/main/java/aetna/com/aetna/AetnaApplication.java
@@ -2,9 +2,19 @@ package aetna.com.aetna;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.
 
 @SpringBootApplication
 public class AetnaApplication {
+	
+@RequestMapping("/")
+    String home() {
+        return "Welcome to Aetna USA!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(AetnaApplication.class, args);
